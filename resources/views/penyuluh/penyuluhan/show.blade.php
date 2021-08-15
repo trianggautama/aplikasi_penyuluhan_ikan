8ac0d31a12ff7ffaac52f4e09f5e477cdbf3f868
tri angga utama <trianggategarutama@gmail.com>
2021-08-15 18:18:52 +0800

    btn

--- resources/views/penyuluh/penyuluhan/show.blade.php
+++ resources/views/penyuluh/penyuluhan/show.blade.php
@@ -21,7 +21,7 @@
                             <form action="{{Route('report.peserta.filter')}}" action="GET">
                                 <input type="hidden" name="punyuluhan_id" value="{{$data->id}}">
                                 <a href="{{Route('report.detail_penyuluhan',$data->id)}}" class="btn btn-sm btn-primary" target="__blank"><i class="fa fa-print"></i> Detail Penyuluhan</a>
-                                <button type="submit" class="btn btn-sm btn-primary" target="__blank"><i class="fa fa-users"></i> Cetak Peserta Kegiatan Penyuluahan</button>
+                                <!-- <button type="submit" class="btn btn-sm btn-primary" target="__blank"><i class="fa fa-users"></i> Cetak Peserta Kegiatan Penyuluahan</button> -->
                                 <a href="{{Route('userPenyuluh.penyuluhan_penyuluh.index')}}" class="btn btn-sm btn-secondary"><i class="fa fa-arrow-left"></i> kembali</a>
                             </form>
                             
